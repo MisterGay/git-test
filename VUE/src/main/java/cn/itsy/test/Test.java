9cd6d248d68c5c624834a331812cfc342654cb14
MasterGay <MasterGay@qq.com>
2019-07-19 22:50:26 +0800

    第一次提交vue项目 提交内容添加了一个Test.java测试类

---
+++ VUE/src/main/java/cn/itsy/test/Test.java
@@ -0,0 +1,12 @@
+package cn.itsy.test;
+
+/**
+ * @description:
+ * @author: MisterGay
+ * @date: 2019-07-19 22:44
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("------------");
+    }
+}
